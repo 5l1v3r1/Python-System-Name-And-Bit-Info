f7a06135a716aa8453664b26f9ead61856fec5b4
İSMAİL TAŞDELEN <pentestdatabase@gmail.com>
2016-05-20 09:04:51 -0400

    Update system_bit.py

--- system_bit.py
+++ system_bit.py
@@ -1,21 +1,36 @@
 #!/usr/bin/env python
 # -*- coding: utf-8 -*-
 
+import sys
 import platform
 
+info_ico = """
+		  GH0ST-S0FTWARE
+# ------------------------------------------------#
+# - Işletim Sistemi 	|         Çıktı          -#
+# ------------------------------------------------#
+# -  32 bit Linux 	| ('32bit', 'ELF')       -#
+# -  64 bit Linux 	| ('64bit', 'ELF')       -#
+# - 32 bit Windows 	| ('32bit', 'WindowsPE') -#
+# - 64 bit Windows 	| ('64bit', 'WindowsPE') -#
+# ------------------------------------------------#
+"""
+print info_ico
+
+islem = input("Yapılcak işlem numarasını giriniz : ")
+
 def system_name_and_bit_info():
-	info_ico = """
-			  GH0ST-S0FTWARE
-	# ------------------------------------------------#
-	# - Işletim Sistemi 	|         Çıktı          -#
-	# ------------------------------------------------#
-	# -  32 bit Linux 	| ('32bit', 'ELF')       -#
-	# -  64 bit Linux 	| ('64bit', 'ELF')       -#
-	# - 32 bit Windows 	| ('32bit', 'WindowsPE') -#
-	# - 64 bit Windows 	| ('64bit', 'WindowsPE') -#
-	# ------------------------------------------------#
-	"""
-	print info_ico
 	print "İşletim sisiteminiz : ", platform.architecture()
 
-system_name_and_bit_info()
+if islem == 1:
+	system_name_and_bit_info()
+	
+elif == 2:
+	sys.exit()
+	
+else:
+	if islem != 1 and 2 and 3 and 4:  
+		print star
+		hata_mesaji = "Gerçersiz işlem numarsı girdiniz. Tekrar deneyin.";
+		print hata_mesaji
+		print star
